087f8c3fbfad3173c2c06669e60dff24545a8384
javaTpoint <javatpoint@xyz>
2023-07-27 10:24:36 +0300

    djangoproject

---
+++ peterdjang/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from .models import People
+
+
+def insertData(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+
+        query = People.objects.create(name=name, email=email, age=age, gender=gender)
+        query.save()
+        return redirect("/")
+    return render(request, "index.html")
+
+
+def indexpage(request):
+    data = People.objects.all()
+    context = {"data": data}
+    return render(request, "index.html", context)
+
+
+# function to Delate data
+def deletedata(request, id):
+    d = People.objects.get(id=id)
+    d.delete()
+    return redirect("/")
+
+    return render(request, "index.html")
+
+
+# function to update record
+def updateData(request, id):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+
+        edit_data = People.objects.get(id=id)
+        edit_data.name = name
+        edit_data.email = email
+        edit_data.age = age
+        edit_data.gender = gender
+        edit_data.save()
+        return redirect("/")
+
+    dta = People.objects.get(id=id)
+    context = {"dta": dta}
+    return render(request, "edit.html", context)
